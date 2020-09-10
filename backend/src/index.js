09d80f14d14fe0a9f7d176b539b6a89d32cd7c9b
David Stjernqvist <david.stjernqvist@se.experis.com>
2020-09-10 16:21:58 +0200

    Added message func and get all users

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+const fireBaseAdmin = require("../config/firebase");
+let defaultAuth = fireBaseAdmin.auth();
+
 router.get('/', (req, res) => {
-    res.send({response: "I am alive"}).status(200);
-  })
-  
+  res.send({ response: "I am alive" }).status(200);
+})
+
+router.get('/api/users', async(req, res) => {
+  const users = [];
+  await defaultAuth.listUsers(1000)
+    .then((listUsersResult) => {
+      listUsersResult.users.forEach((userRecord) => {
+        if (!users.includes(userRecord.email))
+          users.push(userRecord);
+      });
+    })
+    .catch(function (error) {
+      console.log('Error listing users:', error);
+    })
+    res.send(users);
+})
+
+
 module.exports = router;
